2e31c9c5895d927b12cb0e25683783981c3f9170
messense <messense@icloud.com>
2019-01-11 11:18:43 +0800

    Install wget in manylinux.sh

--- manylinux.sh
+++ manylinux.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 # Install dependencies needed by our wheel
-yum -y install gcc libffi-devel xz
+yum -y install gcc libffi-devel xz wget
 
 wget -O /tmp/file.tar.xz http://archive.ubuntu.com/ubuntu/pool/main/f/file/file_5.32.orig.tar.xz
 cd /tmp && xz -cd /tmp/file.tar.xz | tar x && cd -
