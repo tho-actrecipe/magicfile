19bcb36ff129bc173c2c5f8acb3d54796e3333ae
messense <messense@icloud.com>
2020-03-04 10:19:19 +0800

    Update file to 5.38

--- manylinux.sh
+++ manylinux.sh
@@ -4,9 +4,9 @@ set -e -x
 # Install dependencies needed by our wheel
 yum -y install gcc libffi-devel xz wget
 
-wget -O /tmp/file.tar.xz http://archive.ubuntu.com/ubuntu/pool/main/f/file/file_5.32.orig.tar.xz
+wget -O /tmp/file.tar.xz http://archive.ubuntu.com/ubuntu/pool/main/f/file/file_5.38.orig.tar.xz
 cd /tmp && xz -cd /tmp/file.tar.xz | tar x && cd -
-cd /tmp/file-5.32 && ./configure && make && make install && cd -
+cd /tmp/file-5.38 && ./configure && make && make install && cd -
 
 for PYBIN in /opt/python/cp3*/bin; do
     "${PYBIN}/python" setup.py bdist_wheel
