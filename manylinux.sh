b14406f5cc3398c0c3cc0d07d4d07092e3ecf6b5
messense <messense@icloud.com>
2020-03-04 10:44:10 +0800

    Migrate to GitHub Actions

--- manylinux.sh
+++ manylinux.sh
@@ -2,10 +2,10 @@
 set -e -x
 
 # Install dependencies needed by our wheel
-yum -y install gcc libffi-devel xz wget
+yum -y install gcc libffi-devel wget
 
-wget -O /tmp/file.tar.xz http://archive.ubuntu.com/ubuntu/pool/main/f/file/file_5.38.orig.tar.xz
-cd /tmp && xz -cd /tmp/file.tar.xz | tar x && cd -
+wget -O /tmp/file.tar.gz http://archive.ubuntu.com/ubuntu/pool/main/f/file/file_5.38.orig.tar.gz
+cd /tmp && tar zxf file.tar.gz && cd -
 cd /tmp/file-5.38 && ./configure && make && make install && cd -
 
 for PYBIN in /opt/python/cp3*/bin; do
@@ -14,4 +14,5 @@ done
 
 for whl in dist/magicfile*.whl; do
     auditwheel repair "$whl" -w dist/
+    rm "$whl"
 done
