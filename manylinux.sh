7d4efdd80d39848adc55364e896aecd050fb0d54
Tho Nguyen <tho.nguyen@actrecipe.com>
2024-12-03 23:05:44 +0900

    Revert

--- manylinux.sh
+++ manylinux.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
 set -e -x
 
-# Install system dependencies required for building the wheel
-yum -y install gcc libffi-devel wget tar gzip
+# Install dependencies needed by our wheel
+yum -y install gcc libffi-devel wget
 
 # Download and install the correct version of the 'file' utility
-FILE_VERSION="5.38"
-wget -O /tmp/file.tar.gz "http://archive.ubuntu.com/ubuntu/pool/main/f/file/file_${FILE_VERSION}.orig.tar.gz"
-cd /tmp
-tar -zxf file.tar.gz
-cd "file-${FILE_VERSION}"
-./configure
-make
-make install
-cd -
+wget -O /tmp/file.tar.gz http://archive.ubuntu.com/ubuntu/pool/main/f/file/file_5.38.orig.tar.gz
+cd /tmp && tar zxf file.tar.gz && cd -
+cd /tmp/file-5.38 && ./configure && make && make install && cd -
 
-# Build Python wheels for all available Python versions
+# Continue with your original script
 for PYBIN in /opt/python/cp3*/bin; do
-    "${PYBIN}/pip" install -U setuptools wheel  # Ensure setuptools and wheel are up-to-date
     "${PYBIN}/python" setup.py bdist_wheel
 done
 
-# Use auditwheel to repair built wheels
 for whl in dist/actrecipemagicfile*.whl; do
     auditwheel repair "$whl" -w dist/
     rm "$whl"
 done
-
-# Clean up temporary files
-rm -rf /tmp/file*
